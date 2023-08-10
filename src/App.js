7de8dc4e5558b2961d7bc5ef742affbdaf4afe25
cealach <cealach.oconnor@cattleeye.com>
2023-08-10 10:35:54 +0100

    feat: get json placeholder api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Feed from './Components/Feed';
 
-function App() {
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setArticles(response.data);
+      })
+      .catch(error => {
+        console.log('Error fetching articles', error);
+      })
+  }, []);
+
   return (
     <div>
-      
+      <Feed articles={articles} />
     </div>
   );
 }
