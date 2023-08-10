867cbd10a7e545bd74fc869f1dddca7763413801
cealach <cealach.oconnor@cattleeye.com>
2023-08-10 10:36:09 +0100

    fix: display for articles

---
+++ src/Components/Feed.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Styles/Feed.css'
+
+const Articles = ({articles}) => {
+  return (
+    <div className='news-feed'>
+      <h1>News Feed</h1>
+      <div className='articles'>
+        {articles.map((article, index) => (
+          <div key={index} className='article'>
+            <h2>{article.title}</h2>
+            <p>{article.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Articles;
