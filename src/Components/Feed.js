b853d11aab2ebb860b872d0008ee6cd83f8a2d27
cealach <cealach.oconnor@cattleeye.com>
2023-08-10 10:52:30 +0100

    fix: error in map

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -6,7 +6,7 @@ const Articles = ({articles}) => {
     <div className='news-feed'>
       <h1>News Feed</h1>
       <div className='articles'>
-        {articles.map((article, index) => (
+        {articles && articles.map((article, index) => (
           <div key={index} className='article'>
             <h2>{article.title}</h2>
             <p>{article.body}</p>
