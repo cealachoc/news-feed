01db4d69ab7e1be55d5be0c7593aab0b9036db02
cealach <cealach.oconnor@cattleeye.com>
2023-08-10 11:33:26 +0100

    chore: remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import Feed from './Components/Feed';
+import Fetch from './Components/Fetch';
 
 
 const App = () => {
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => {
-        setArticles(data);
-      })
-      .catch(error => {
-        console.log("Error fetching articles: " + error);
-      });
-  }, []);
 
   return (
     <div>
-      <Feed articles={articles} />
+      <Fetch />
     </div>
   );
 }
