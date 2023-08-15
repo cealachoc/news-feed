dbc1d8cd962dfb2a7929a95470332eed10c8c946
cealach <cealach.oconnor@cattleeye.com>
2023-08-15 16:40:45 +0100

    feat: sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Fetch from './Components/Fetch';
-
+import Sidebar from './Components/Sidebar';
 
 const App = () => {
 
   return (
-    <div>
-      <Fetch />
+    <div style={{display: 'flex'}}>
+        <Sidebar />
+      <div style={{flex: '1', padding: '20px'}} >
+        <Fetch />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import Feed from './Feed';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Home from '@mui/icons-material/Home';
+import Favorite from '@mui/icons-material/Favorite';
+import Message from '@mui/icons-material/Message';
+import { Explore } from '@mui/icons-material';
+import { AccountCircle } from '@mui/icons-material';
+
+const drawerWidth = 240;
+
+export default function Sidebar() {
+  const icons = [
+    <Home />,
+    <Explore />,
+    <Favorite />,
+    <Message />,
+    <AccountCircle />
+  ];
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {['Home', 'Explore', 'Likes', 'Messages', 'Profile'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {icons[index]}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
