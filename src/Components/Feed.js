d09369844fdfb21ca4e8712b803e717205e210b2
cealach <cealach.oconnor@cattleeye.com>
2023-08-15 14:52:10 +0100

    feat: circular progress every 10 cards on scroll

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -5,15 +5,16 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import CircularProgress from '@mui/joy/CircularProgress';
 
 export default function Feed({ articles, photos, comments, setArticles }) {
-  const cardsPerRow = 3;
+  const articlesPerPage = 10;
+  const [visibleArticles, setVisibleArticles] = React.useState(articles.slice(0, articlesPerPage));
+  const [isLoading, setIsLoading] = React.useState(false);
 
-  const rows = [];
-  for (let i = 0; i < articles.length; i += cardsPerRow) {
-    const row = articles.slice(i, i + cardsPerRow);
-    rows.push(row);
-  }
+  React.useEffect(() => {
+    setVisibleArticles(articles.slice(0, articlesPerPage));
+  }, [articles]);
 
   const handleToggleComments = (articleIndex) => {
     const updatedArticles = articles.map((article, index) => {
@@ -26,10 +27,30 @@ export default function Feed({ articles, photos, comments, setArticles }) {
       return article;
     });
     setArticles(updatedArticles);
-  }
+  };
 
-    return (
-      <div
+  const handleScroll = () => {
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollTop = window.scrollY;
+
+    if (documentHeight - (windowHeight + scrollTop) < 100) {
+      setIsLoading(true);
+      const startIndex = visibleArticles.length;
+      const endIndex = startIndex + articlesPerPage;
+      setVisibleArticles([...visibleArticles, ...articles.slice(startIndex, endIndex)]);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [visibleArticles]);
+
+  return (
+    <div
       style={{
         display: 'flex',
         flexDirection: 'column',
@@ -37,60 +58,60 @@ export default function Feed({ articles, photos, comments, setArticles }) {
         minHeight: '100vh',
         padding: '20px',
       }}
-      >
+    >
       <h1>News feed</h1>
-      {rows.map((row, rowIndex) => (
+      {visibleArticles.map((article, index) => (
         <div
-        key={rowIndex}
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          marginBottom: '20px',
-        }}
+          key={index}
+          style={{
+            marginBottom: '20px',
+          }}
         >
-          {row.map((article, columnIndex) => {
-            const photo = photos[columnIndex];
-            const articleComments = comments.filter(comment => comment.postId === article.id);
-            return (
-            <Card
-              key={columnIndex}
-              sx={{ maxWidth: 300, width: '100%', marginRight: '20px' }}
-            >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={photo.url}
-                  alt="Article Photo"
-                  />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {article.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {article.body}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary" onClick={() => handleToggleComments(columnIndex)}>
-                  {article.showComments ? 'Hide Comments' : 'View Comments'}
-                </Button>
-              </CardActions>
-              {article.showComments && (
-                <CardContent>
-              {articleComments.map(comment => (
-                <div key={comment.id}>
-                  <h3>Comment by {comment.name}</h3>
-                  <p>{comment.body}</p>
-                </div>
-              ))}
+          <Card
+            sx={{ maxWidth: 300, width: '100%' }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={photos[index].url}
+                alt="Article Photo"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {article.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {article.body}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary" onClick={() => handleToggleComments(index)}>
+                {article.showComments ? 'Hide Comments' : 'View Comments'}
+              </Button>
+            </CardActions>
+            {article.showComments && (
+              <CardContent>
+                {comments
+                  .filter(comment => comment.postId === article.id)
+                  .map(comment => (
+                    <div key={comment.id}>
+                      <h3>Comment by {comment.name}</h3>
+                      <p>{comment.body}</p>
+                    </div>
+                  ))}
               </CardContent>
             )}
-            </Card>
-)})}
+          </Card>
         </div>
       ))}
+
+      {isLoading && (
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}} >
+          <CircularProgress variant='soft' color='neutral'/>
+        </div>
+      )}
     </div>
   );
 }
