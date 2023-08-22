c7a1b10398a778554f0d01657df8b4a4f37a9152
cealach <cealach.oconnor@cattleeye.com>
2023-08-22 16:02:39 +0100

    chore: remove unused code

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import Feed from './Feed';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
