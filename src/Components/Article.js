1f032609497d0e90a9e0055b516dae6548725311
cealach <cealach.oconnor@cattleeye.com>
2023-08-23 11:51:42 +0100

    fix: create single reusable Article

---
+++ src/Components/Article.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import ArrowUpward from '@mui/icons-material/ArrowUpward';
+import ArrowDownward from '@mui/icons-material/ArrowDownward';
+
+const Article = ({ article, photo, fetchComments }) => {
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  const toggleShowComments = () => {
+    if (!showComments) {
+      fetchComments(article.id)
+        .then(data => setComments(data))
+        .catch(error => console.error('Error fetching comments', error))
+    }
+    setShowComments(!showComments)
+  };
+
+  return (
+    <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+      <Card
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          margin: '0 10px 20px',
+        }}
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="400"
+            image={photo.url}
+            alt="Article Photo"
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component="div">
+              {article.title}
+            </Typography>
+            <Typography variant='body2' color="text.secondary">
+              {article.body}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            size='small'
+            color='primary'
+            onClick={toggleShowComments}>
+              {showComments ? <ArrowUpward /> : <ArrowDownward />}
+              {showComments ? 'Hide Comments' : 'View Comments'}
+            </Button>
+        </CardActions>
+        {showComments && (
+          <CardContent
+            sx={{
+              maxHeight: '200px',
+              overflowY: 'auto'
+            }}
+          >
+            {comments.map(comment => (
+              <div key={comment.id}>
+                <h3>Comment by: {comment.name}</h3>
+                <p>{comment.body}</p>
+              </div>
+            ))}
+          </CardContent>
+        )}
+      </Card>
+    </div>
+  )
+}
+
+export default Article;
--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,78 +1,32 @@
-import React, { useState } from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import ArrowUpward from '@mui/icons-material/ArrowUpward';
-import ArrowDownward from '@mui/icons-material/ArrowDownward';
+import React from 'react';
+import Article from './Article';
 
-export default function Articles({ articles, photos, handleToggleComments }) {
-  const [comments, setComments] = useState([]);
+export default function Articles({ articles, photos}) {
 
-  const fetchComments = () => {
-    fetch('https://jsonplaceholder.typicode.com/comments')
-      .then(response => response.json())
-      .then(data => setComments(data))
-      .catch(error => console.error('Error fetching comments:', error));
+  const fetchComments = async (postId) => {
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
   };
 
   return (
-    <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+    <div style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center'
+    }}>
       {articles.map((article, index) => (
-        <div key={index} style={{ marginBottom: '20px', width: '80%', maxWidth: '100%' }}>
-          <Card
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              margin: '0 10px 20px',
-            }}
-            >
-            <CardActionArea>
-              <CardMedia component="img" height="400" image={photos[index].url} alt="Article Photo" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {article.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {article.body}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                onClick={() => {
-                  handleToggleComments(index);
-                  fetchComments();
-              }}
-            >
-              {article.showComments ? <ArrowUpward /> : <ArrowDownward />}
-              {article.showComments ? 'Hide Comments' : 'View Comments'}
-              </Button>
-            </CardActions>
-            {article.showComments && (
-              <CardContent
-                sx={{
-                  maxHeight: '200px',
-                  overflowY: 'auto'
-                }}
-              >
-                {comments
-                  .filter(comment => comment.postId === article.id)
-                  .map(comment => (
-                    <div key={comment.id}>
-                      <h3>Comment by: {comment.name}</h3>
-                      <p>{comment.body}</p>
-                    </div>
-                  ))}
-              </CardContent>
-            )}
-          </Card>
-        </div>
+        <Article
+          key={index}
+          article={article}
+          photo={photos[index]}
+          fetchComments={fetchComments}
+        />
       ))}
-    <div/>
-  </div>
-  );
+    </div>
+  )
 }
